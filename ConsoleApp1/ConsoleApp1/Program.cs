74255c7cf1a519afdff7234a4758aa1ed0cf69be
Playingtygre <kymiehsu123@gmail.com>
2018-03-21 13:42:56 -0700

    conecting unit test with main console app

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,25 +2,25 @@
 
 namespace ConsoleApp1
 {
-    class Program
+    public class Program
     {
-        //allows access to to anyting 
+
         public static void Main()
         {
 
             int amount = 1000, deposit, withdraw;
-            int choice, pin = 0, x = 0;
+            int choice, pin = 0;
             Console.WriteLine("Enter your Pin Number");
             pin = int.Parse(Console.ReadLine());
 
             // if user input the correct pin this while loop executes. 
             while (true)
             {
-                Console.WriteLine(" ----Welcom to the bank----\n ");
-                Console.WriteLine("Current Balance{0}", amount);
-                Console.WriteLine("1. Check Your Balance \n");
-                Console.WriteLine("2. Take Money out of your account \n");
-                Console.WriteLine("3. Add money to your account \n ");
+                Console.WriteLine(" ----Welcom to the bank---- ");
+                Console.WriteLine("Current Balance {0}", amount);
+                Console.WriteLine("1. Check Your Balance");
+                Console.WriteLine("2. Take Money out of your account");
+                Console.WriteLine("3. Add money to your account ");
                 Console.WriteLine("4. Exit -- Quit ");
 
 
@@ -30,32 +30,36 @@ namespace ConsoleApp1
                 {
                     // If use press one then balance is displayed
                     case 1:
-                        Console.WriteLine("\n Your balance in R's: {0}", amount);
+                        Console.WriteLine(" Your balance is : {0}", amount);
                         break;
 
                     //If user press 2 then amount is asked to make an subtraction
                     case 2:
-                        Console.WriteLine("\n Enter The Amount to Withdraw: ");
+                        Console.WriteLine("Enter The Amount to Withdraw: ");
                         withdraw = int.Parse(Console.ReadLine());
+
                         if (withdraw % 100 != 0)
                         {
-                            Console.WriteLine("/n Please enter Multiples of ten");
+                            Console.WriteLine("Please enter Multiples of 100");
                         }
-                        else if (withdraw > (amount - 500))
+
+                        // user input cannot allow more than the given balance
+                        else if (withdraw > (amount - 0))
                         {
-                            Console.WriteLine("\n INSUFFICENT BALANCE");
+                            Console.WriteLine("INSUFFICENT BALANCE");
                         }
+
                         else
                         {
                             amount = amount - withdraw;
-                            Console.WriteLine("\n\n Please Collect Cash");
-                            Console.WriteLine("\n Your balance is {0}", amount);
+                            Console.WriteLine(" Please Collect Cash");
+                            Console.WriteLine("Your balance is {0}", amount);
                         }
                         break;
 
                      // pressing 1 allows user to add money to account
                     case 3:
-                        Console.WriteLine("\n Enter the Ammount to Deposit");
+                        Console.WriteLine("Enter the Ammount to Deposit");
                         deposit = int.Parse(Console.ReadLine());
                         amount = deposit + amount;
                         Console.WriteLine("Your blanace is {0}", amount);
@@ -63,13 +67,13 @@ namespace ConsoleApp1
 
         
                     case 4:
-                        Console.WriteLine("\n thank you for using this Console ATM");
+                        Console.WriteLine(" Thank you for using this Console ATM");
                         break;
 
-                
+        
                 }
 
-                Console.WriteLine("\n\n Transaction ended");
+                Console.WriteLine("Transaction ended");
 
             }
 
